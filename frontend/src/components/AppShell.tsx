35cd0bb9e07008b66f470f2236e599436b3d89e9
MaybeRichard <lisicheng168@gmail.com>
2026-03-06 01:18:11 +0800

    auto: update frontend/src/components/AppShell.tsx

--- frontend/src/components/AppShell.tsx
+++ frontend/src/components/AppShell.tsx
@@ -13,6 +13,7 @@ interface AppShellProps {
   activeProjectId: number | null;
   projects: Project[];
   onProjectCreated: (p: Project) => void;
+  onRefreshProjects?: () => void;
   connectionStatus: "connected" | "disconnected" | "connecting";
 }
 
