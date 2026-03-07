0e7901ec3d75027fe4ed2430c5117a0e26b0dcd0
MaybeRichard <lisicheng168@gmail.com>
2026-03-07 09:02:26 +0800

    auto: update frontend/src/modules/perf/cards/DiskCard.tsx

--- frontend/src/modules/perf/cards/DiskCard.tsx
+++ frontend/src/modules/perf/cards/DiskCard.tsx
@@ -1,4 +1,5 @@
 // src/modules/perf/cards/DiskCard.tsx
+import { useTranslation } from "react-i18next";
 import type { PerfData } from "../types";
 import { ICard, DualSparkline, fmtGb, fmtMbps, CGR } from "./shared";
 
