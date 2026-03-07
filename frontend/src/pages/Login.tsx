31277f1968e081a5f7949bec81bc748aa9fa8ae6
MaybeRichard <lisicheng168@gmail.com>
2026-03-07 08:57:55 +0800

    auto: update frontend/src/pages/Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 // frontend/src/pages/Login.tsx
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { authWithPin, saveConfig } from "../lib/api";
