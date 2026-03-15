99490266cb607ddd158a8a585ca6552f941bf89d
MaybeRichard <lisicheng168@gmail.com>
2026-03-16 00:56:24 +0800

    auto: update tauri/src/ui/icon/icons/user.tsx

---
+++ tauri/src/ui/icon/icons/user.tsx
@@ -0,0 +1,4 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconUser(props: IconProps) {
+  return <Icon {...props}><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></Icon>
+}
