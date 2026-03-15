5a85ca4f0ce5eeed46615d6bec4bb6f9be2ba616
MaybeRichard <lisicheng168@gmail.com>
2026-03-16 00:50:35 +0800

    auto: update tauri/src/main.tsx

--- tauri/src/main.tsx
+++ tauri/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { AppRouter } from './app/Router'
 import { Providers } from './app/Providers'
 import './i18n'
-import './index.css'
+import './styles/reset.css'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
