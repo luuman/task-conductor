b9022b9174b6ed2d79fe431383b437f9c22a77f0
MaybeRichard <lisicheng168@gmail.com>
2026-03-16 01:04:53 +0800

    auto: update tauri/src/app/PageLoading.tsx

--- tauri/src/app/PageLoading.tsx
+++ tauri/src/app/PageLoading.tsx
@@ -1,7 +1,9 @@
+import styles from './page-loading.module.css'
+
 export function PageLoading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-background">
-      <div className="h-8 w-8 animate-spin rounded-full border-2 border-muted border-t-foreground" />
+    <div className={styles.container}>
+      <div className={styles.spinner} />
     </div>
   )
 }
