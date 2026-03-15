7a3e81c2e8a7209f291e48a28aa12ae4505b160f
MaybeRichard <lisicheng168@gmail.com>
2026-03-16 00:56:14 +0800

    auto: update tauri/src/ui/icon/icons/x.tsx

---
+++ tauri/src/ui/icon/icons/x.tsx
@@ -0,0 +1,4 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconX(props: IconProps) {
+  return <Icon {...props}><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></Icon>
+}
