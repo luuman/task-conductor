c0e674686b17724e0c5cbeaf4303dc33d94c3f6b
MaybeRichard <lisicheng168@gmail.com>
2026-03-16 23:12:33 +0800

    auto: update tauri/src/ui/icon/icons/arrow-left.tsx

---
+++ tauri/src/ui/icon/icons/arrow-left.tsx
@@ -0,0 +1,4 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconArrowLeft(props: IconProps) {
+  return <Icon {...props}><line x1="19" y1="12" x2="5" y2="12" /><polyline points="12 19 5 12 12 5" /></Icon>
+}
