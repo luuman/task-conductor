fca3e16c2c1b0ae7fd3d74f6a93e3f798ee70165
MaybeRichard <lisicheng168@gmail.com>
2026-03-17 22:55:57 +0800

    auto: update tauri/src/ui/icon/icons/globe.tsx

---
+++ tauri/src/ui/icon/icons/globe.tsx
@@ -0,0 +1,10 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconGlobe(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <path d="M2 12h20" />
+      <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+    </Icon>
+  )
+}
