c89223d047cfdd41ad37d2a0253c28a52fe8418f
MaybeRichard <lisicheng168@gmail.com>
2026-03-17 23:07:22 +0800

    auto: update tauri/src/features/admin/pages/AdminSessions.tsx

--- tauri/src/features/admin/pages/AdminSessions.tsx
+++ tauri/src/features/admin/pages/AdminSessions.tsx
@@ -8,7 +8,7 @@ import hljs from 'highlight.js/lib/core'
 import '../../../styles/hljs-ayu-dark.css'
 import {
   IconTerminal, IconFileText, IconPencil, IconWrench, IconSearch,
-  IconFolderOpen, IconGlobe, IconBot, IconCircleHelp,
+  IconFolderOpen, IconGlobe, IconBot, IconCircleHelp, IconChevronDown,
   IconChevronRight, IconUser,
 } from '../../../ui/icon'
 import typescript from 'highlight.js/lib/languages/typescript'
