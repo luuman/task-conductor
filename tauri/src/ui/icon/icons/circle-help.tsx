612649053bb4f8241140d8fc3f55e021761f9167
MaybeRichard <lisicheng168@gmail.com>
2026-03-17 22:56:01 +0800

    auto: update tauri/src/ui/icon/icons/circle-help.tsx

---
+++ tauri/src/ui/icon/icons/circle-help.tsx
@@ -0,0 +1,10 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconCircleHelp(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+      <path d="M12 17h.01" />
+    </Icon>
+  )
+}
