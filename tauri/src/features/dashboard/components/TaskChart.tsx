ce781806e6ceb9bf13aaaf528ca3e8b36f5d195a
MaybeRichard <lisicheng168@gmail.com>
2026-03-20 21:27:46 +0800

    auto: update tauri/src/features/dashboard/components/TaskChart.tsx

--- tauri/src/features/dashboard/components/TaskChart.tsx
+++ tauri/src/features/dashboard/components/TaskChart.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 import type { Task } from '../../../lib/api/types'
 import { Skeleton } from '../../../ui/skeleton/Skeleton'
 import styles from './TaskChart.module.css'
