db405b21070cc6e54b4e6e667d18dd714116ecf2
MaybeRichard <lisicheng168@gmail.com>
2026-03-21 00:17:56 +0800

    auto: update tauri/src/app/Router.tsx

--- tauri/src/app/Router.tsx
+++ tauri/src/app/Router.tsx
@@ -21,6 +21,7 @@ const FilesPage         = lazy(() => import('../features/files'))
 const GitPage           = lazy(() => import('../features/git'))
 const TaskManagerPage   = lazy(() => import('../features/task-manager'))
 const TaskDetailPage    = lazy(() => import('../features/task-detail'))
+const CanvasPage        = lazy(() => import('../features/canvas'))
 
 function PlaceholderPageComponent() {
   const { t } = useTranslation()
