4ffbcf97a81df3a9a9de679abc76f213697de539
MaybeRichard <lisicheng168@gmail.com>
2026-03-23 00:37:41 +0800

    auto: update tauri/src/features/__dev__/tabs/ChatDemo.tsx

--- tauri/src/features/__dev__/tabs/ChatDemo.tsx
+++ tauri/src/features/__dev__/tabs/ChatDemo.tsx
@@ -470,7 +470,7 @@ function ChatDemoCanvas() {
       </ReactFlow>
       {/* SVG 连线覆盖在 ReactFlow 上方 */}
       <SvgLines pairs={pairs} />
-      <FloatingNav sections={DEMO_SECTIONS} onJump={handleJump} cardStyle={cardStyle} onStyleChange={setCardStyle} />
+      <FloatingNav sections={DEMO_SECTIONS} onJump={handleJump} />
     </div>
     </CardStyleCtx.Provider>
   )
