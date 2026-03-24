f928b6ae822db57430346270464053e98c005fdc
MaybeRichard <lisicheng168@gmail.com>
2026-03-24 16:18:54 +0800

    auto: update tauri/src/features/chat/index.tsx

--- tauri/src/features/chat/index.tsx
+++ tauri/src/features/chat/index.tsx
@@ -2,7 +2,7 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { api } from '../../lib/api'
 import type { AiSession, TranscriptMessage } from '../../lib/api/types'
-import { parseTimeline, formatTs, guessLang, guessHljsLang, type TimelineStep } from './timeline-parser'
+import { parseTimeline, formatTs, guessLang, guessHljsLang, guessBashOutputLang, type TimelineStep } from './timeline-parser'
 import { useHighlight } from '../../lib/useHighlight'
 import '../../styles/hljs-ayu-dark.css'
 import s from './chat-report.module.css'
