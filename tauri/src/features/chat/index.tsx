44f8c5558ce109c18f0c7411bfbac200280cee80
MaybeRichard <lisicheng168@gmail.com>
2026-03-24 22:53:40 +0800

    auto: update tauri/src/features/chat/index.tsx

--- tauri/src/features/chat/index.tsx
+++ tauri/src/features/chat/index.tsx
@@ -667,6 +667,7 @@ export default function ChatReportPage() {
 
       <div className={s.body}>
         <div className={s.mainArea} ref={mainAreaRef}>
+          <CodeExpandCtx.Provider value={codeExpanded}>
           <InspectCtx.Provider value={inspectCtx}>
             {loading ? (
               <div className={s.empty}><span>加载中...</span></div>
