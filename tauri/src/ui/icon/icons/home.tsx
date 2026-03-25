e255077db1ced31a158ae3e3c40d62a31222d595
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 11:53:43 +0800

    auto: update tauri/src/ui/icon/icons/home.tsx

---
+++ tauri/src/ui/icon/icons/home.tsx
@@ -0,0 +1,9 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconHome(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </Icon>
+  )
+}
