b9f0d4860b8da8374743c0e3831bbb348e6958ca
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 19:09:37 +0800

    auto: update tauri/src/components/FloatingAssistant/FloatingAssistant.tsx

--- tauri/src/components/FloatingAssistant/FloatingAssistant.tsx
+++ tauri/src/components/FloatingAssistant/FloatingAssistant.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useRef, useState, type KeyboardEvent } from 'react'
-import { IconBot, IconX, IconMinus, IconMaximize, IconClock } from '../../ui/icon'
+import { IconX, IconMinus, IconMaximize, IconClock } from '../../ui/icon'
 import { useChatStore, type PageContext } from '../../lib/store/chat'
 import { useChatStream } from '../../hooks/useChatStream'
 import { useAppStore } from '../../lib/store/app'
