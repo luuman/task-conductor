80f4f820d1a8ace97cd95adc39d753d088f5fbc5
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 12:24:07 +0800

    auto: update tauri/src/ui/icon/icons/clock.tsx

---
+++ tauri/src/ui/icon/icons/clock.tsx
@@ -0,0 +1,9 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconClock(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <polyline points="12 6 12 12 16 14" />
+    </Icon>
+  )
+}
