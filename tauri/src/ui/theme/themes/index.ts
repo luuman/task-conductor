6d4a41d2abf9b32cb34c260469d23ff230087707
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 13:50:50 +0800

    auto: update tauri/src/ui/theme/themes/index.ts

---
+++ tauri/src/ui/theme/themes/index.ts
@@ -0,0 +1,30 @@
+import type { ThemeJSON } from '../tokens'
+import darkPlus  from './dark-plus.json'
+import lightPlus from './light-plus.json'
+import onyx      from './onyx.json'
+import jade      from './jade.json'
+import rose      from './rose.json'
+import amber     from './amber.json'
+import coral     from './coral.json'
+import teal      from './teal.json'
+import crimson   from './crimson.json'
+import lime      from './lime.json'
+import lavender  from './lavender.json'
+import midnight  from './midnight.json'
+import pitch     from './pitch.json'
+
+export const ALL_THEMES: ThemeJSON[] = [
+  darkPlus  as ThemeJSON,
+  lightPlus as ThemeJSON,
+  onyx      as ThemeJSON,
+  jade      as ThemeJSON,
+  rose      as ThemeJSON,
+  amber     as ThemeJSON,
+  coral     as ThemeJSON,
+  teal      as ThemeJSON,
+  crimson   as ThemeJSON,
+  lime      as ThemeJSON,
+  lavender  as ThemeJSON,
+  midnight  as ThemeJSON,
+  pitch     as ThemeJSON,
+]
