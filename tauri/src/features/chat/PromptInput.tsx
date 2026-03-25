c2bb6f518a6947209fdf33a426671d6fb6b54064
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 23:19:55 +0800

    auto: update tauri/src/features/chat/PromptInput.tsx

--- tauri/src/features/chat/PromptInput.tsx
+++ tauri/src/features/chat/PromptInput.tsx
@@ -390,7 +390,10 @@ export function PromptInput() {
         {isPicking && (
           <div className={s.pPickingHint}>
             <IconCrosshair size={12} />
-            <span>点击页面任意元素以选取，<kbd>Esc</kbd> 取消</span>
+            {domCtxList.length > 0
+              ? <span>已选 <strong>{domCtxList.length}</strong> 个，继续点击选更多，<kbd>Esc</kbd> 完成</span>
+              : <span>点击页面任意元素以选取，<kbd>Esc</kbd> 取消</span>
+            }
           </div>
         )}
 
