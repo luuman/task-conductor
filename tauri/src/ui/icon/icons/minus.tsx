b6e1216cc8afa6058cf2c25d687c1547b3a7e092
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 12:24:08 +0800

    auto: update tauri/src/ui/icon/icons/minus.tsx

---
+++ tauri/src/ui/icon/icons/minus.tsx
@@ -0,0 +1,8 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconMinus(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <line x1="5" x2="19" y1="12" y2="12" />
+    </Icon>
+  )
+}
