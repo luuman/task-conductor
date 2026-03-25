bd856257bfe07f95e0dd71365016b0cf87331f99
MaybeRichard <lisicheng168@gmail.com>
2026-03-25 11:50:32 +0800

    auto: update tauri/src/components/FloatingAssistant/FloatingAssistant.tsx

--- tauri/src/components/FloatingAssistant/FloatingAssistant.tsx
+++ tauri/src/components/FloatingAssistant/FloatingAssistant.tsx
@@ -409,14 +409,6 @@ export function FloatingAssistant() {
 
   return (
     <>
-      {!isOpen && (
-        <button className={styles.fab} onClick={toggle} title="AI 助手 (Ctrl+J)">
-          <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
-          </svg>
-        </button>
-      )}
-
       {isOpen && (
         <div
           ref={panelRef}
