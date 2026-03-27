97fb79291bde1ff752f74a7a60d1c5a94acc6f03
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 22:56:44 +0800

    auto: update tauri/src/features/chat/index.tsx

--- tauri/src/features/chat/index.tsx
+++ tauri/src/features/chat/index.tsx
@@ -15,7 +15,7 @@ import {
 } from '../../ui/icon'
 import { Virtuoso, VirtuosoHandle } from 'react-virtuoso'
 import { useTranslation } from 'react-i18next'
-import { useSessionData } from '../../components/SessionChat/useSessionData'
+import { useSessionData, type QuestionItem } from '../../components/SessionChat/useSessionData'
 import { useAppStore } from '../../lib/store/app'
 import '../../styles/hljs-ayu-dark.css'
 import s from './chat-report.module.css'
