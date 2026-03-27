e02a390d6d34f9da4a95e7916f02bf9018131c51
zhongcheng.li <luumans@qq.com>
2026-03-27 11:58:27 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   .prettierrc
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   .vscode/settings.json
    	modified:   tauri/package.json
    	modified:   tauri/src/styles/global.css
    	modified:   tauri/vite.config.ts

--- tauri/vite.config.ts
+++ tauri/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0',
-    port: 7071,
+    port: 1420,
     proxy: {
       '/api':    { target: 'http://localhost:8765', changeOrigin: true },
       '/auth':   { target: 'http://localhost:8765', changeOrigin: true },
