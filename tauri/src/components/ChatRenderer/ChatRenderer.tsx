3537998cdb5203b52fdc41c495fdfca94c851f1e
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 23:06:40 +0800

    auto: update tauri/src/components/ChatRenderer/ChatRenderer.tsx

--- tauri/src/components/ChatRenderer/ChatRenderer.tsx
+++ tauri/src/components/ChatRenderer/ChatRenderer.tsx
@@ -8,6 +8,7 @@ import remarkGfm from 'remark-gfm'
 import type { Components } from 'react-markdown'
 import hljs from 'highlight.js/lib/core'
 import '../../styles/hljs-ayu-dark.css'
+import { cleanSystemXml } from '../../features/chat/timeline-parser'
 import { useHighlight } from '../../lib/useHighlight'
 import {
   IconTerminal, IconFileText, IconPencil, IconWrench, IconSearch,
