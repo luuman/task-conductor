13b1467d14c692d64e4bfe600164b3902ea21ca6
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 22:35:00 +0800

    auto: update tauri/src/ui/icon/icons/hook.tsx

---
+++ tauri/src/ui/icon/icons/hook.tsx
@@ -0,0 +1,4 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconHook(props: IconProps) {
+  return <Icon {...props}><path d="M18 13v-2a4 4 0 0 0-4-4H6l4-4M6 7l4 4" /><path d="M18 13a4 4 0 0 1-4 4H6" /></Icon>
+}
