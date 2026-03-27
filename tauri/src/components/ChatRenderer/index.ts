9ef50248ccd83256a331a79f9662b4a470d573e2
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 14:52:13 +0800

    auto: update tauri/src/components/ChatRenderer/index.ts

--- tauri/src/components/ChatRenderer/index.ts
+++ tauri/src/components/ChatRenderer/index.ts
@@ -16,6 +16,7 @@ export {
   DiffBlock,
   fileExtIcon,
   CodeExpandCtx,
+  parseTextSegments,
 } from './ChatRenderer'
 
 export type { ChatMessageListProps, AssistantTurn, GroupedTurnItem, CodeBlockVariant } from './ChatRenderer'
