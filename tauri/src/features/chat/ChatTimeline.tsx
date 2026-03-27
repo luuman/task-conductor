7083f58282fb5aa04a4b3770b20c57decdc6c270
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 17:59:50 +0800

    auto: update tauri/src/features/chat/ChatTimeline.tsx

--- tauri/src/features/chat/ChatTimeline.tsx
+++ tauri/src/features/chat/ChatTimeline.tsx
@@ -4,7 +4,7 @@
  */
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import type { TranscriptMessage } from '../../lib/api/types'
-import { parseTimelineWithQuestions, guessHljsLang } from './timeline-parser'
+import { parseTimelineWithQuestions, guessHljsLang, cleanSystemXml } from './timeline-parser'
 import type { TimelineStep } from './timeline-parser'
 import { RichTextBlock, CodeBlock, DiffBlock, fileExtIcon, CodeExpandCtx } from '../../components/ChatRenderer'
 import {
