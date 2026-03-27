427b40550d493cc70d1871ec007fa79249309896
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 22:35:02 +0800

    auto: update tauri/src/ui/icon/icons/lock.tsx

---
+++ tauri/src/ui/icon/icons/lock.tsx
@@ -0,0 +1,4 @@
+import { Icon, type IconProps } from '../Icon'
+export function IconLock(props: IconProps) {
+  return <Icon {...props}><rect width="18" height="11" x="3" y="11" rx="2" ry="2" /><path d="M7 11V7a5 5 0 0 1 10 0v4" /></Icon>
+}
