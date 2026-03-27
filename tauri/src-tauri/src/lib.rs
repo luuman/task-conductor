03d804424a6ba4bf9fec6f83d92d9a8a14b46469
MaybeRichard <lisicheng168@gmail.com>
2026-03-27 17:22:15 +0800

    auto: update tauri/src-tauri/src/lib.rs

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -260,6 +260,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .manage(FileCache::new())
         .invoke_handler(tauri::generate_handler![
             greet,
