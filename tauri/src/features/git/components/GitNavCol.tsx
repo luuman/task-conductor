52d743d6c4a80bd63e48dd2d8e9b9fea7ab884a9
MaybeRichard <lisicheng168@gmail.com>
2026-03-31 23:46:07 +0800

    auto: update tauri/src/features/git/components/GitNavCol.tsx

--- tauri/src/features/git/components/GitNavCol.tsx
+++ tauri/src/features/git/components/GitNavCol.tsx
@@ -63,7 +63,7 @@ export function GitNavCol({ onConfigClick }: Props) {
   return (
     <aside className={css.nav}>
       <div className={css.repoHdr}>
-        <svg width="12" height="12" viewBox="0 0 16 16" fill="var(--tc-success,#4ade80)">
+        <svg width="12" height="12" viewBox="0 0 16 16" fill="var(--tc-success)">
           <path d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
         </svg>
         <span className={css.repoName}>task-conductor</span>
