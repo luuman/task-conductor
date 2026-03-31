68d37201134be7a1b4e6dc94651826b8f9bc9373
MaybeRichard <lisicheng168@gmail.com>
2026-04-01 00:03:14 +0800

    auto: update tauri/src/features/git/components/DiffViewer.tsx

--- tauri/src/features/git/components/DiffViewer.tsx
+++ tauri/src/features/git/components/DiffViewer.tsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { DiffEditor, Editor, type Monaco, type OnMount, type BeforeMount } from '@monaco-editor/react'
+import { DiffEditor, Editor, type Monaco, type BeforeMount } from '@monaco-editor/react'
 import type { editor } from 'monaco-editor'
 import { useQuery } from '@tanstack/react-query'
 import { useGitStore } from '../../../lib/store/git'
