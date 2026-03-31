f8f2295495eec6d5cf3d23e681d99b6a8c35c884
MaybeRichard <lisicheng168@gmail.com>
2026-03-31 23:38:05 +0800

    auto: update tauri/src/features/git/components/ChangesPanel.tsx

--- tauri/src/features/git/components/ChangesPanel.tsx
+++ tauri/src/features/git/components/ChangesPanel.tsx
@@ -193,7 +193,7 @@ export function ChangesPanel() {
       {/* Branch footer */}
       <div className={css.branchFooter}>
         <div className={css.branchName}>
-          <svg width="10" height="10" viewBox="0 0 16 16" fill="#4ec9b0">
+          <svg width="10" height="10" viewBox="0 0 16 16" fill="var(--tc-success,#4ade80)">
             <path d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
           </svg>
           <span className={css.branchLabel}>{currentBranch}</span>
